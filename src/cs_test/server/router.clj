bf9122fc68783f8bcd3a4a4370f1d27b31716033
Rusty Bentley <mycoding.alert@gmail.com>
2016-03-22 18:20:18 -0500

    adding refs to project, starting socket work

--- src/cs_test/server/router.clj
+++ src/cs_test/server/router.clj
@@ -1,4 +1,19 @@
-(ns cs-test.server.router)
+(ns cs-test.server.router
+  (:require
+    [taoensso.sente :as sente]
+    [taoensso.sente.server-adapters.http-kit :as http-kit]
+ ))
+
+(declare channel-socket)
+
+(defn start-websocket []
+  (defonce channel-socket
+    (sente/make-channel-socket!
+      http-kit/sente-web-server-adapter
+      {})))
+
+
+
 
 (defn init []
   (println "creating socket...")
