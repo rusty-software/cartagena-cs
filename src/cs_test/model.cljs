502de6d4203b26814a63596b842301a8f0431cfe
Rusty Bentley <mycoding.alert@gmail.com>
2016-03-25 12:45:17 -0500

    add game-token model update

--- src/cs_test/model.cljs
+++ src/cs_test/model.cljs
@@ -9,3 +9,9 @@
 
 (defn update-player-name! [name]
   (swap! game-state update-player-name name))
+
+(defn update-game-token [gs token]
+  (assoc gs :game-token token))
+
+(defn update-game-token! [token]
+  (swap! game-state update-game-token token))
